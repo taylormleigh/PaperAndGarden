97814aa551b505328186c7397eb44a8f7ff9f879
Tay <taylor.mleigh@gmail.com>
2021-10-30 15:42:14 -0500

    text on hover for button navigation

--- components/footer.js
+++ components/footer.js
@@ -4,11 +4,27 @@ const Footer = () => {
   return (
     <div id="footer">
       <div id="buttonNavigation">
-        <span id="linktreeButton"></span>
-        <span id="githubButton"></span>
-        <span id="tayMadeButton"></span>
-        <span id="linkedInButton"></span>
-        <span id="contactButton"></span>
+
+        <span id="linktreeButton">
+          <span className="buttonHoverText">Writing Resources</span>
+        </span>
+
+        <span id="githubButton">
+          <span className="buttonHoverText">Github Repo</span>
+        </span>
+
+        <span id="tayMadeButton">
+          <span className="buttonHoverText">Portfolio</span>
+        </span>
+
+        <span id="linkedInButton">
+          <span className="buttonHoverText">Linked In</span>
+        </span>
+
+        <span id="contactButton">
+          <span className="buttonHoverText">Contact</span>
+        </span>
+
       </div>
     </div>
   )
