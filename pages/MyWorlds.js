68ffb29f90c9c6aad005270da9fb473f1a970fb0
Tay <taylor.mleigh@gmail.com>
2021-10-30 06:09:14 -0500

    organizing divs in main pages

--- pages/MyWorlds.js
+++ pages/MyWorlds.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout.js';
 // import WorldList from '../components/worldList';
 
 export default function MyWorlds({logIn}) {
