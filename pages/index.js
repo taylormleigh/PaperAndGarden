0dc0a04dbbcfa6e70fc4386df5091a0b7a774663
Tay <taylor.mleigh@gmail.com>
2021-10-30 04:02:58 -0500

    outlining main pages

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import Layout from "../components/layout";
+import Layout from "../components/Layout.js";
 
 export default function Home({newWorld}) {
 
   return (
     <Layout>
-      <div id="home">
+      <div>
 
-          <h1>Paper & Garden</h1>
-
+        <div id="logo"></div>
+        <div id="homeQuote">
           <div className="quote"><p /><i>“I think there are two types of writers, the architects and the gardeners. The architects plan everything ahead of time, like an architect building a house. They have the whole thing designed and blueprinted out before they even nail the first board up. The gardeners dig a hole, drop in a seed and water it. As the plant comes up and they water it, they don't know how many branches it's going to have, they find out as it grows."</i>
           <br />-George R.R. Martin</div>
+        </div>
+        <div id="bodyTextHome">
+        <p />Paper & Garden is a service that assists storytellers in developing their setting. 
 
-          <p />Paper & Garden is a free service that assists storytellers in developing their setting. Whether you're designing a story or planting it, your locations can be as rich as characters. Each region and city has a unique personality that interacts with your story and the people within it. Help to maintain consistency by saving the details of those places here for easy reference.
+        <p /> Whether you're meticulously designing a story or simply planting its seed to watch it grow, your locations can be as rich as characters. Each area of your world has a unique personality that interacts with your characters, informs your plot, and illustrates your themes. 
 
-          <p /><button className="headerButton" onClick={newWorld}>BEGIN</button>
-          <p />
+        <p/>We can help you discover the world your story deserves.
+        </div>
+
+          
       </div>
     </Layout>
     );
