d6e97b80aa098509ff199b6ac6832d5bdc5dbe20
Tay <taylor.mleigh@gmail.com>
2021-11-01 15:35:43 -0500

    footer styling

--- components/footer.js
+++ components/footer.js
@@ -3,27 +3,37 @@ import React from 'react';
 const Footer = () => {
   return (
     <div id="footer">
-      <div id="buttonNavigation">
+      <div id="buttonNavigation" className="row">
 
-        <a id="linktreeButton" href="https://linktr.ee/paper.garden">
+      <span className="col-xs">
+        <a id="linktreeButton" className="footerButton" href="https://linktr.ee/paper.garden">
           <span className="buttonHoverText">Writing Resources</span>
         </a>
+      </span>
 
-        <a id="githubButton" href="https://github.com/taylormleigh/PaperAndGarden">
+      <span className="col-xs">
+        <a id="githubButton" className="footerButton" href="https://github.com/taylormleigh/PaperAndGarden">
           <span className="buttonHoverText">Github Repo</span>
         </a>
+      </span>
 
-        <a id="tayMadeButton" href="https://tay-made.com">
+      <span className="col-xs">
+        <a id="tayMadeButton" className="footerButton" href="https://tay-made.com">
           <span className="buttonHoverText">Portfolio</span>
         </a>
+      </span>
 
-        <a id="linkedInButton" href="https://www.linkedin.com/in/tay-huval/">
+      <span className="col-xs">
+        <a id="linkedInButton" className="footerButton" href="https://www.linkedin.com/in/tay-huval/">
           <span className="buttonHoverText">Linked In</span>
         </a>
+      </span>
 
-        <a id="contactButton" href="mailto:me@tay-made.com">
-          <span className="buttonHoverText">Contact</span>
+      <span className="col-xs">
+        <a id="contactButton" className="footerButton" href="mailto:me@tay-made.com">
+          <span className="buttonHoverText">Contact Me</span>
         </a>
+      </span>
 
       </div>
     </div>
