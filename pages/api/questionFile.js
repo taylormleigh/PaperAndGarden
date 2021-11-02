1294762ffc7d00ff5790cb4a4a22233894cddc6e
Tay <taylor.mleigh@gmail.com>
2021-11-02 13:11:07 -0500

    making questions into a built in object file rather than pulling from a database

---
+++ pages/api/questionFile.js
@@ -0,0 +1,197 @@
+let world = {
+  section: "world",
+  "world name": "",
+  subsections : {
+    "world basics": [
+      {
+        question: "Name your world",
+        placeholder: "world name",
+        type: "textarea",
+        answer: ""
+      },
+      {
+        question: "How many days are in the year?",
+        placeholder: "revolution",
+        type: "textarea",
+        answer: ""
+      },
+      {
+        question: "How many hours are in a day?",
+        placeholder: "rotation",
+        type: "textarea",
+        answer: ""
+      },
+      
+    ],
+    regions: [
+      
+    ],
+  }
+}
+
+let region = {
+  section: "region",
+  "region name": "",
+  subsections : {
+    geography: [
+      {
+        question: "Name your Region",
+        placeholder: "region name",
+        type: "textarea",
+        answer: ""
+      },
+      {
+        question: "Where is this region located within your world?",
+        placeholder: "northern hemisphere by equator",
+        type: "textarea",
+        answer: ""
+      },
+      // {
+      //   question: "What type of habitat is it?",
+      //   "placeholder": ["desert", "tundra", "grassland", "forest", "rainforest", "wetland", "aquatic"],
+      //   type: "dropdown",
+      //   answer: ""
+      // },
+    ],
+    "flora & fauna": [
+      {
+        question: "What are the richest natural resources of this area?",
+        placeholder: "minerals, crops, materials, etc.",
+        type: "textarea",
+        answer: ""
+      },
+      {
+        question: "What resources are most scarce?",
+        placeholder: "water, wildlife, fertile land, etc.",
+        type: "textarea",
+        answer: ""
+      },
+    ],
+    cities: [
+
+    ]
+  }
+}
+
+let city = {
+  section: "city",
+  "city name": "",
+  subsections: {
+    basics: [
+      {
+        question: "What is the name of your city?",
+        placeholder: "city name",
+        type: "textarea",
+        answer: ""
+      }
+    ]
+  }
+}
+
+
+module.exports = {
+  world,
+  region,
+  city
+}
+
+// "0": {
+//   "section": "Planet",
+//   "subsection": "World Basics",
+//   "questions": [
+//     {
+//       "question": "Name your world",
+//       "placeholder": "world name",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "How many days are in the year?",
+//       "placeholder": "revolution",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "How many hours are in a day?",
+//       "placeholder": "rotation",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "What are the seasons?",
+//       "placeholder": "Does it vary by region? How long does each season last?",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "How many suns & moons?",
+//       "placeholder": "Do these celestial bodies have names or special meaning to the inhabitants of your planet?",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "Could this world exist with our current laws of physics or is there a fundamental difference?",
+//       "placeholder": "spell out the logic if it differs from the universe we know, i.e. magic is real and energy can come from nothing",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "What other quirks about this reality exist?",
+//       "placeholder": "humans evolved to have gills and can breathe and live underwater",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "Describe the intelligent lifeforms that exist on this planet",
+//       "placeholder": "if more than one race, how and when did each race evolve? Their general appearance/s? Average lifespan/s? Unique facets? Relationship to other races?",
+//       "type": "textarea"
+//     }
+//   ]
+// },
+
+// "1": {
+//   "section": "Region",
+//   "subsection": "Geography",
+//   "questions": [
+//     {
+//       "question": "Name your Region",
+//       "placeholder": "region name",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "Where is this region located within your world?",
+//       "placeholder": "northern hemisphere by equator",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "What type of habitat is it?",
+//       "placeholder": ["desert", "tundra", "grassland", "forest", "rainforest", "wetland", "aquatic"],
+//       "type": "dropdown"
+//     },
+//     {
+//       "question": "What is the climate?",
+//       "placeholder": ["dry", "polar", "continental", "temperate", "tropical"],
+//       "type": "dropdown"
+//     },
+//     {
+//       "question": "What is unique about the weather in this region?",
+//       "placeholder": "prone to tornados, extreme temperature, will go from sunny to storming on a dime, etc.",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "What are the most notable points of geography?",
+//       "placeholder": "mountains, bodies of water, volcanoes, etc.",
+//       "type": "textarea"
+//     }
+//   ]
+// },
+
+// "2": {
+//   "section": "Region",
+//   "subsection": "Flora & Fauna",
+//   "questions": [
+//     {
+//       "question": "What are the richest natural resources of this area?",
+//       "placeholder": "minerals, crops, materials, etc.",
+//       "type": "textarea"
+//     },
+//     {
+//       "question": "What resources are most scarce?",
+//       "placeholder": "water, wildlife, fertile land, etc.",
+//       "type": "textarea"
+//     }
+//   ]
+// }
