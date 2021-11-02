28715258e814dd7074bcba436d765d4527e140c5
Tay <taylor.mleigh@gmail.com>
2021-11-01 21:35:53 -0500

    mobile responsive footer

--- components/footer.js
+++ components/footer.js
@@ -3,33 +3,33 @@ import React from 'react';
 const Footer = () => {
   return (
     <div id="footer">
-      <div id="buttonNavigation" className="row">
+      <div id="buttonNavigation">
 
-      <span className="col-xs">
+      <span className="col-xs footerButtonContainer">
         <a id="linktreeButton" className="footerButton" href="https://linktr.ee/paper.garden">
           <span className="buttonHoverText">Writing Resources</span>
         </a>
       </span>
 
-      <span className="col-xs">
+      <span className="col-xs footerButtonContainer">
         <a id="githubButton" className="footerButton" href="https://github.com/taylormleigh/PaperAndGarden">
           <span className="buttonHoverText">Github Repo</span>
         </a>
       </span>
 
-      <span className="col-xs">
+      <span className="col-xs footerButtonContainer">
         <a id="tayMadeButton" className="footerButton" href="https://tay-made.com">
           <span className="buttonHoverText">Portfolio</span>
         </a>
       </span>
 
-      <span className="col-xs">
+      <span className="col-xs footerButtonContainer">
         <a id="linkedInButton" className="footerButton" href="https://www.linkedin.com/in/tay-huval/">
           <span className="buttonHoverText">Linked In</span>
         </a>
       </span>
 
-      <span className="col-xs">
+      <span className="col-xs footerButtonContainer">
         <a id="contactButton" className="footerButton" href="mailto:me@tay-made.com">
           <span className="buttonHoverText">Contact Me</span>
         </a>
