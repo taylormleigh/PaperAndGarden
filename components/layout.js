dd68f7731502f39eb9c52d01a4c86eaac2765356
Tay <taylor.mleigh@gmail.com>
2021-11-02 20:50:28 -0500

    cleaned code on layout

--- components/layout.js
+++ components/layout.js
@@ -3,51 +3,6 @@ import Header from "./Header"
 import Footer from "./Footer"
 import Navigation from "./Navigation"
 
-// const Layout = ({children}) => {
-//   return (
-//     <>
-//       <div className="row container">
-
-//         <div className="row">
-//           <div className="col-xs-12 col-sm-8 col-md-6 col-lg-4">
-//             <div className="box">
-//               <Header />
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="row">
-//           <div className="col-xs-12 col-sm-8 col-md-6 col-lg-4">
-//             <div className="box">
-//               <Navigation />
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="row">
-//           <div className="col-xs-12 col-sm-8 col-md-6 col-lg-4">
-//             <div className="box">
-//               <div className="bodyDiv">
-//                 <main>{children}</main>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div className="row">
-//           <div className="col-xs-12 col-sm-8 col-md-6 col-lg-4">
-//             <div className="box">
-//               <Footer />
-//             </div>
-//           </div>
-//         </div>
-
-
-//       </div>
-//     </>
-//   )
-// }
-
 const Layout = ({children}) => {
 
   return (
