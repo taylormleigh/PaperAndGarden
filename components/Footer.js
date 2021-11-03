9865c15d1ce6acdf491bc8f03be68667e29f2c1c
Tay <taylor.mleigh@gmail.com>
2021-11-03 18:31:31 -0500

    added motion to footer navigation buttons

--- components/Footer.js
+++ components/Footer.js
@@ -1,39 +1,50 @@
 import React from 'react';
+import {motion, useMotionValue, useTransform} from 'framer-motion';
 
 const Footer = () => {
   return (
     <div id="footer">
       <div id="buttonNavigation">
 
-      <span className="col-xs footerButtonContainer">
+      <motion.div 
+        whileHover={{ scale: 1.05 }}
+        className="col-xs footerButtonContainer">
         <a id="linktreeButton" className="footerButton" href="https://linktr.ee/paper.garden">
           <span className="buttonHoverText">Writing Resources</span>
         </a>
-      </span>
+      </motion.div>
 
-      <span className="col-xs footerButtonContainer">
+      <motion.div 
+        whileHover={{ scale: 1.05 }}
+        className="col-xs footerButtonContainer">
         <a id="githubButton" className="footerButton" href="https://github.com/taylormleigh/PaperAndGarden">
           <span className="buttonHoverText">Github Repo</span>
         </a>
-      </span>
+      </motion.div>
 
-      <span className="col-xs footerButtonContainer">
+      <motion.div 
+        whileHover={{ scale: 1.05 }} 
+        className="col-xs footerButtonContainer">
         <a id="tayMadeButton" className="footerButton" href="https://tay-made.com">
           <span className="buttonHoverText">Portfolio</span>
         </a>
-      </span>
+      </motion.div>
 
-      <span className="col-xs footerButtonContainer">
+      <motion.div 
+        whileHover={{ scale: 1.05 }}
+        className="col-xs footerButtonContainer">
         <a id="linkedInButton" className="footerButton" href="https://www.linkedin.com/in/tay-huval/">
           <span className="buttonHoverText">Linked In</span>
         </a>
-      </span>
+      </motion.div>
 
-      <span className="col-xs footerButtonContainer">
+      <motion.div 
+        whileHover={{ scale: 1.05 }} 
+        className="col-xs footerButtonContainer">
         <a id="contactButton" className="footerButton" href="mailto:me@tay-made.com">
           <span className="buttonHoverText">Contact Me</span>
         </a>
-      </span>
+      </motion.div>
 
       </div>
     </div>
