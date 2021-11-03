a2f9cf73b65c4c5006c425de09e31dd4c3f480a3
Tay <taylor.mleigh@gmail.com>
2021-11-02 22:05:11 -0500

    troubleshooting favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles.css'
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <link rel="icon" href="/favicon.png" />
+    </Head>
     <Component {...pageProps} />
+  </>
 )
 
 export default MyApp;
